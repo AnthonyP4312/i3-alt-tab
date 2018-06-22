ba92bb0a2972f190e23ab03f3c7ee4b130556982
Anthony Palmer <AnthonyP4312@gmail.com>
2018-06-21 23:46:52 -0400

    My god that was difficult

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,51 @@ extern crate serde;
 
 pub mod i3 {
 
-    #[derive(Serialize, Deserialize, Debug)]
+    #[derive(Serialize, Deserialize, Debug, Clone)]
     pub struct Node {
-        id: u8,
-        name: String,
+        pub name: Option<String>,
+        // pub id: u64,
         #[serde(rename = "type")]
-        type_con: Type,
-        border: String,
-
+        pub type_con: Type,
+        // pub current_border_width: i32,
+        pub layout: Layout,
+        // pub rect: Rect,
+        pub focused: bool,
+        // pub floating_nodes: Vec<Node>,
+        pub window: Option<u64>,
+        pub nodes: Vec<Node>,
     }
 
-    #[derive(Serialize, Deserialize, Debug)]
+
+
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    #[serde(rename_all = "snake_case")]
     pub enum Type {
-        #[serde(rename_all = "snake_case")]
         Root,
         Output,
         Con,
         FloatingCon,
         Workspace,
-        Dockarea
+        Dockarea,
+    }
+
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    #[serde(rename_all = "snake_case")]
+    pub enum Layout {
+        Splith,
+        Splitv,
+        Stacked,
+        Tabbed,
+        Dockarea,
+        Output
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Rect {
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::process::Command;
-use std::str::from_utf8;
+use std::cell::RefCell;
 
 extern crate serde_json;
+extern crate alt_tabber;
 
+use alt_tabber::i3::{ Node, Type };
 use serde_json::{from_slice};
 
 
@@ -13,7 +15,48 @@ fn main() {
         .output()
         .unwrap();
 
-    let output_str = from_utf8(&output.stdout).unwrap();
-    println!("{:?}", output_str);
+    let nodes: Node = from_slice(&output.stdout).unwrap();
+    // println!("{:#?}", nodes);
 
+    let ws = RefCell::new(&nodes);
+    let result = RefCell::new(&nodes);
+
+    find_focused_ws(&nodes, &ws, &result);
+    println!("Focuses Workspace: {:?}", result);
+    // Find Focused node
+
+    // From there find parent workspace
+
+    // Build up focused array
+}
+
+fn flattenNodes(root: &Node) {
+
+    for node in root.nodes.iter() {
+        match node {
+            Node { focused: true, ..} => println!("Focused Node: {:?}", node),
+            Node { type_con: Type::Workspace, ..} => println!("Workspace Node {:?}", node),
+            _ => println!("{:?}", root),
+        }
+
+        flattenNodes(&node)
+    }
+}
+
+fn find_focused_ws<'a>(root: &'a Node, ws: &RefCell<&'a Node>, result: &RefCell<&'a Node>) {
+    for node in root.nodes.iter() {
+        match node {
+            Node { focused: true, ..} => {
+                println!("Focused Node: {:?}", node);
+                *result.borrow_mut() = &ws.borrow();
+                break;
+            },
+            Node { type_con: Type::Workspace, ..} => {
+                println!("Workspace Node {:?}", node);
+                *ws.borrow_mut() = &node
+            },
+            _ => println!("{:?}", root),
+        }
+        find_focused_ws(&node, ws, result);
+    }
 }
