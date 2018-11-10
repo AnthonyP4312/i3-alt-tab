fe7da7ef5c22aa31181fc68c9ce476062c2efe4d
Anthony Palmer <AnthonyP4312@gmail.com>
2018-11-10 15:24:01 -0500

    wip moving clones to references

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::VecDeque;
+use std::env;
 use std::process::Command;
 
-extern crate serde_json;
 extern crate alt_tabber;
+extern crate serde_json;
 
-use alt_tabber::i3::{ Node, Type };
-use serde_json::{from_slice};
-
+use alt_tabber::i3::{Node, Type};
+use serde_json::from_slice;
 
 fn main() {
     let output = Command::new("i3-msg")
@@ -18,7 +18,7 @@ fn main() {
     let nodes: Node = from_slice(&output.stdout).unwrap();
 
     // From there find parent workspace
-    let ws = queued_ws(nodes);
+    let ws = queued_ws2(&nodes).unwrap();
 
     // Build up array of windows on focused WS
     let mut windows = flatten_children(ws);
@@ -29,10 +29,16 @@ fn main() {
 
     // Find the next in line (wrapping to front if last window)
     let focused_index = windows.iter().position(|n| n.focused).unwrap();
-    let next = if focused_index == 0 {
-        windows.iter().last().unwrap()
-    } else {
-        windows.iter().nth(focused_index - 1).unwrap()
+
+    // Determine whether we're moving back or forwards
+    let args: Vec<String> = env::args().collect();
+
+    let next = match args[1].as_ref() {
+        "prev" if focused_index == 0 => windows.iter().last().unwrap(),
+        "prev" => windows.iter().nth(focused_index - 1).unwrap(),
+        "next" if focused_index == windows.len() => windows[0],
+        "next" => windows.iter().nth(focused_index + 1).unwrap(),
+        _ => panic!("Must provide either next or prev as an argument"),
     };
 
     // Send Message
@@ -41,29 +47,31 @@ fn main() {
         .output();
 }
 
-fn flatten_children(ws: Node) -> Vec<Node> {
+fn flatten_children(ws: &Node) -> Vec<&Node> {
     let mut children = vec![];
-    let mut queue: VecDeque<Node> = VecDeque::new();
+    let mut queue: VecDeque<&Node> = VecDeque::new();
     queue.push_back(ws);
     loop {
         let this_node = queue.pop_back().unwrap();
         for node in this_node.nodes.iter() {
             match node {
-                Node { window: Some(_), ..} => {
-                    children.push(node.clone());
-                },
-                _ => queue.push_back(node.clone())
+                Node {
+                    window: Some(_), ..
+                } => children.push(node),
+                _ => queue.push_back(node),
             }
         }
-        if queue.is_empty() { break; }
+        if queue.is_empty() {
+            break;
+        }
     }
     children
 }
 
 #[cfg(test)]
 mod test_flatten {
-    use alt_tabber::i3::Rect;
     use super::*;
+    use alt_tabber::i3::Rect;
     fn setup() {
         let child_a = Node {
             name: Some(String::from("child_a")),
@@ -73,31 +81,25 @@ mod test_flatten {
                 x: 0,
                 y: 0,
                 height: 0,
-                width: 0
+                width: 0,
             },
             focused: true,
             window: Some(123),
-            nodes: vec![]
+            nodes: vec![],
         };
-        let child_b = Node {
-            ..child_a.clone()
-        };
+        let child_b = Node { ..child_a.clone() };
         let child_c = Node {
             nodes: vec![child_a.clone(), child_b],
             ..child_a.clone()
         };
-        let node_with_children = Node {
-            ..child_a.clone()
-        };
-        return ()
+        let node_with_children = Node { ..child_a.clone() };
+        return ();
     }
     #[test]
-    fn flattens() {
-
-    }
+    fn flattens() {}
 }
 
-fn queued_ws(root: Node) -> Node{
+fn queued_ws(root: Node) -> Node {
     // Lots of cloning, probably a better way to do this.
     let mut queue: VecDeque<Node> = VecDeque::new();
     let mut ws: Node = root.clone();
@@ -108,14 +110,71 @@ fn queued_ws(root: Node) -> Node{
             queue.push_back(node.clone());
         }
         match this_node {
-            Node { focused: true, ..} => {
+            Node { focused: true, .. } => {
                 break;
-            },
-            Node { type_con: Type::Workspace, ..} => {
-                ws = this_node
-            },
+            }
+            Node {
+                type_con: Type::Workspace,
+                ..
+            } => ws = this_node,
             _ => (),
         }
     }
     ws
+}
+
+// Maybe try it with references
+fn queued_ws2(root: &Node) -> Option<&Node> {
+    // I am bad at the code
+    let mut queue: VecDeque<&Node> = VecDeque::new();
+    let mut ws: Option<&Node> = None;
+    queue.push_back(root);
+    loop {
+        let this_node = queue.pop_back().unwrap();
+        for node in this_node.nodes.iter() {
+            queue.push_back(node);
+        }
+        match this_node {
+            Node { focused: true, .. } => {
+                break;
+            }
+            Node {
+                type_con: Type::Workspace,
+                ..
+            } => ws = Some(this_node),
+            _ => (),
+        }
+    }
+    ws
+}
+
+fn recursive_ws<'a>(root: &'a Node, parent: Option<&'a Node>) -> Option<&'a Node> {
+    let focused_child = root.nodes.iter().find(|n| n.focused);
+
+    match focused_child {
+        // If we find a focused child, bubble up this parent
+        Some(_) => parent,
+        // If none of the nodes children are focused then dig deeper
+        None => {
+            let children: Vec<Option<&Node>> = root.nodes
+                .iter()
+                .map(|r| recursive_ws(r, Some(root)))
+                .collect();
+            let focus = children.iter().find(|&n| match n {
+                Some(_) => true,
+                None => false,
+            });
+            println!(
+                "{:?}",
+                match parent {
+                    Some(x) => Some(&x.name),
+                    None => None,
+                }
+            );
+            match focus {
+                Some(_) => parent,
+                None => None,
+            }
+        }
+    }
 }
